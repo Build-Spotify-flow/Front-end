e350bf477f04d9a6d094396d1a72a63563c1e5cf
agohorel <gohorelalex@gmail.com>
2020-01-10 09:34:40 -0500

    update spotify redirect in app.js

--- song_suggester/src/App.js
+++ song_suggester/src/App.js
@@ -24,7 +24,7 @@ function App() {
 
   const baseUrl = "https://accounts.spotify.com/authorize";
   const clientID = "46e78711e2c14c21a689149e27b79266";
-  const redirect = "http://localhost:3000/dashboard";
+  const redirect = "https://symphinityengine.netlify.com/dashboard";
   const scope = "user-read-private";
   const url = `${baseUrl}?client_id=${clientID}&redirect_uri=${redirect}&scope=${scope}&response_type=token`;
 
