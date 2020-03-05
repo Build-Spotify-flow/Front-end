e65c06bca75c93231ad4773c40ec7ba95b1a008a
agohorel <3317203+agohorel@users.noreply.github.com>
2020-03-04 20:21:04 -0500

    clear out unused component files

--- song_suggester/src/components/Dashboard.js
+++ song_suggester/src/components/Dashboard.js
@@ -1,53 +1,67 @@
-import React, {useState, useEffect} from 'react';
-import {Route} from 'react-router-dom';
-import {connect} from 'react-redux';
-import Search from './Search';
+import React from "react";
+import { Wrapper, Aside, Nav, Main } from "../stylesheets/Layout";
+import { Search } from "./Search";
+import { SongDetail } from "./SongDetail";
+import { withRouter } from "react-router";
+import { Link } from "react-router-dom";
 
-import SongContainer from "./SongContainer";
-import SongOption from "./SongOption";
+const TempDash = ({
+  selectedSong,
+  setSelectedSong,
+  searchResults,
+  setSearchResults,
+  songData,
+  setSongData,
+  searchTerm,
+  setSearchTerm,
+  history
+}) => {
+  const logout = () => {
+    localStorage.removeItem("token");
+    localStorage.removeItem("spotifyToken");
+    history.push("/login");
+  };
 
-const Dashboard = ({match})=>{
-     
-    const [currentSearch, setCurrentSearch]= useState();
-    const [searchResult, setSearchResult] = useState();
-    const [songSelected, setSongSelected] = useState();
-    const [songOptionSelected, setSongOptionSelected] = useState(false)
-
-    // const testEvent = searchField =>{
-    //     setCurrentSearch(searchField.value);
-    // };
-console.log(searchResult)
-    return(
-        <div className="dashboard-wrapper">
-            <Search
-            type="music"
-            placeholder="Search for a song"
-            eventTimer="800"
-            // songList={search.songList}
-            setSearchResult={setSearchResult}
-            setSongOptionSelected={songOptionSelected}
-            />
-
-            {/* <div className="song-option-container">
-                {
-                    !songOptionSelected && searchResult.map((song, index) => 
-                    <SongOption key={index} song={song} setSongSelected={setSongSelected} setSongOptionSelected={setSongOptionSelected} />)
-            }
-        </div> */}
-        
-        { songOptionSelected && <SongContainer song={songSelected} />}
-
-        {/* <Route path={`${match.url}/profile`} render={ ()=>{
-            // Display Profile Component
-            return(<h2>Profile Component</h2>);
-        }} /> */}
-
-        <Route path={`${match.url}/songs`} render={ () =>{
-            //Display Saved Songs Component
-            return(<h2>Saved Songs Component</h2>);
-        }} />
-    </div>
-);
+  return (
+    <Wrapper>
+      <Aside>
+        <Nav>
+          <Link href="/dashboard" onClick={() => setSelectedSong({})}>
+            <i className="fas fa-columns"></i>Dashboard
+          </Link>
+          <Link to="/favorites">
+            <i className="far fa-heart"></i>Favorites
+          </Link>
+          <Link onClick={() => logout()}>
+            <i className="fas fa-sign-out-alt"></i>Logout
+          </Link>
+        </Nav>
+      </Aside>
+      <Main>
+        <Search
+          searchTerm={searchTerm}
+          setSearchTerm={setSearchTerm}
+          searchResults={searchResults}
+          setSearchResults={setSearchResults}
+          selectedSong={selectedSong}
+          setSelectedSong={setSelectedSong}
+          songData={songData}
+          setSongData={setSongData}
+        ></Search>
+        {!!Object.keys(selectedSong).length && (
+          <SongDetail
+            song={selectedSong}
+            songData={songData}
+            selectedSong={selectedSong}
+            setSelectedSong={setSelectedSong}
+            setSongData={setSongData}
+            setSearchResults={setSearchResults}
+            setSearchTerm={setSearchTerm}
+          ></SongDetail>
+        )}
+      </Main>
+    </Wrapper>
+  );
 };
 
-export default Dashboard;
+export default withRouter(TempDash);
